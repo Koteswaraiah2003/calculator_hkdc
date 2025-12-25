4b44980b7046e0eda54de45189baaed2c73d4081
HruthikTM <135125990+HruthikTM@users.noreply.github.com>
2025-12-25 11:49:06 +0530

    calculator code commit

---
+++ Calculator.cpp
@@ -0,0 +1,43 @@
+#include "Calculator.h"
+
+// Calculator::Calculator() {}
+
+void Calculator::menu()
+{
+    int x = 10, y = 5;
+    Calculator calc;
+    cout << "Addition: " << calc.add(x, y) << endl;
+    cout << "Subtraction: " << calc.subtract(x, y) << endl;
+    cout << "Multiplication: " << calc.multiply(x, y) << endl;
+    cout << "Division: " << calc.divide(x, y) << endl;
+
+}
+
+int Calculator::add(int a, int b)
+{
+    cout << "from Hruthik" << endl;
+    return a + b;
+}
+
+int Calculator::subtract(int a, int b)
+{
+    cout << "from Hruthik" << endl;
+    return a - b;
+}
+
+int Calculator::multiply(int a, int b)
+{
+     cout << "from Hruthik" << endl;
+    return a * b;
+}
+
+double Calculator::divide(int a, int b)
+{
+    cout << "from Hruthik" << endl;
+    if (b == 0)
+    {
+        cout << "Error: Division by zero!" << endl;
+        return 0;
+    }
+    return (a) / b;
+}
